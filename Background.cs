16314c5821d901bc0a781cc4c55320b0ce2180d7
frank <frankliuct@139.com>
2024-11-01 23:19:19 +0800

    创建游戏的背景

---
+++ Background.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThunderFighter
+{
+    class Background : GameObject
+    {
+        private Image img;
+
+        public Background(int x, int y, int width, int height, int speed, int life, Direction dir) : base(x, y, width, height, speed, life, dir)
+        {
+        }
+
+        public override void Draw(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MoveToBorder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
